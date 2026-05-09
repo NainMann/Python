38eee34591bb98d08af61fa5acb1a7850c8df49e
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-05-10 01:09:33 +0530

    code

---
+++ __________ROUGH___________/.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a numpy array
+arr = np.array([1, 2, 3, 4, 5])
+
+# Find sum of all elements
+total = np.sum(arr)
+
+
+# Print the sum
+print("Sum of all elements:", total)
