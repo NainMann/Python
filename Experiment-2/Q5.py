dbae80bc1290ffba1c7f594d3852bf36c9fef38c
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-05-13 19:55:49 +0530

    code

--- Experiment-2/Q5.py
+++ Experiment-2/Q5.py
@@ -7,4 +7,4 @@ if D >= 0:
     r2=-b-(D**0.5)
     print ('The roots of the quadratic are ' , r1 ,'and' , r2)
 else :
-    print('The quadractic have imaginary roots.')    
+    print('The quadractic have imaginary roots.')
--- __________ROUGH___________/.py
+++ __________ROUGH___________/.py
@@ -1,2 +1,5 @@
-n=45
-print(type(n))
+year=int(input('enter the year : '))
+if year%400==0 or year%4==0 and year%100!=0:
+    print('the entered number is leap year ')
+else:
+    print('the number is not leap')
